04e7983f2ca896c0724916720aa0c5066d703523
Stephanie Andrews <seandrews11@gmail.com>
2017-06-26 20:33:15 -0600

    Pass all Objects and Context tests

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      this.name = obj.name
+      this.greeting = obj.greeting
+      this.fn = fn
+      return this.fn()
     },
 
     alterObjects : function(constructor, greeting) {
-
+      this.constructor = constructor
+      this.greeting = greeting
+      constructor.prototype.greeting = greeting
     },
 
     iterate : function(obj) {
-
+      return Object.keys(obj).map(function(key) {
+        return `${key}: ${obj[key]}`
+      })
     }
   };
 });
