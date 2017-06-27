b9a150aaaabf941108fe9d03d19a56cdf19635f6
Stephanie Andrews <seandrews11@gmail.com>
2017-06-26 19:28:45 -0600

    Pass all logical operators tests

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -3,11 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     or : function(a, b) {
-
+      if (a == false && b == false) {
+        return false
+      } else {
+        return true
+      }
     },
 
     and : function(a, b) {
-
+      if (a == true && b == true) {
+        return true
+      } else {
+        return false
+      }
     }
   };
 });
