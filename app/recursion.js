2556a2eedfe9b178b7144124de08fd2e51cf2b22
Stephanie Andrews <seandrews11@gmail.com>
2017-07-02 11:16:26 -0600

    Pass permutation test

--- app/recursion.js
+++ app/recursion.js
@@ -12,7 +12,7 @@ define(function() {
 
         dirs.push( dir.dir );
 
-        for (i = 0 ; i < files.length; i++) {
+        for (var i = 0 ; i < files.length; i++) {
           if (typeof files[i] === 'string') {
             if (!dirName || dirs.indexOf(dirName) > -1) {
               listOfFiles.push(files[i]);
@@ -32,7 +32,28 @@ define(function() {
     },
 
     permute: function(arr) {
+      var temp = [];
+      var answer = [];
 
+      function logResult() {
+        answer.push(temp.slice());
+      }
+
+      function build() {
+        for (var i = 0; i < arr.length ; i++) {
+          var item = arr.splice(i, 1)[0];
+          temp.push(item);
+          if(arr.length) {
+            build();
+          } else {
+            logResult();
+          }
+          arr.splice(i, 0, item);
+          temp.pop();
+        }
+        return answer;
+      }
+      return build();
     }
   };
 });
