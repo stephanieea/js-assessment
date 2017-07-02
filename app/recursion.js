a36bf60d0c1318738f49129b5ee557007f5d5552
Stephanie Andrews <seandrews11@gmail.com>
2017-07-01 19:55:22 -0600

    Pass recursion tests

--- app/recursion.js
+++ app/recursion.js
@@ -3,7 +3,32 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var listOfFiles = [];
+      var dirs = [];
 
+      function processDir(dir) {
+
+        var files = dir.files;
+
+        dirs.push( dir.dir );
+
+        for (i = 0 ; i < files.length; i++) {
+          if (typeof files[i] === 'string') {
+            if (!dirName || dirs.indexOf(dirName) > -1) {
+              listOfFiles.push(files[i]);
+            }
+          } else {
+            processDir(files[i]);
+          }
+        }
+
+        dirs.pop();
+      }
+
+      processDir(data);
+
+
+      return listOfFiles;
     },
 
     permute: function(arr) {
