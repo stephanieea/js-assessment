e5689149558da20c930cd20912026e7aef66eb98
Stephanie Andrews <seandrews11@gmail.com>
2017-07-05 21:09:52 -0600

    Pass closure and partial function tests

--- app/functions.js
+++ app/functions.js
@@ -3,23 +3,39 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+      return fn.apply(null, arr)
     },
 
     speak : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+      return function(arg) {
+        return str + ', ' + arg;
+      };
     },
 
     makeClosures : function(arr, fn) {
+      var ret = [];
 
+      var makeFn = function(val) {
+        return function() {
+          return fn(val);
+        };
+      };
+
+      for (var i = 0; i < arr.length; i++) {
+        ret.push(makeFn(arr[i]));
+      }
+
+      return ret;
     },
 
     partial : function(fn, str1, str2) {
-
+      return function(str3) {
+        return fn(str1, str2, str3)
+      }
     },
 
     useArguments : function() {
