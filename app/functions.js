f3af3f61ee7dd105f9bd85aa433e3631105b8d33
Stephanie Andrews <seandrews11@gmail.com>
2017-07-12 12:01:41 -0600

    Pass all function tests

--- app/functions.js
+++ app/functions.js
@@ -39,19 +39,46 @@ define(function() {
     },
 
     useArguments : function() {
-
+      var sum = 0;
+      for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+      }
+      return sum;
     },
 
     callIt : function(fn) {
-
+      var args = Array.prototype.slice.call(arguments, 1, arguments.length)
+      fn.apply(null, args)
     },
 
     partialUsingArguments : function(fn) {
-
+      var args = Array.prototype.slice.call(arguments, 1, arguments.length)
+      return function() {
+        var moreArgs = args.concat(Array.prototype.slice.call(arguments))
+        return fn.apply(null, moreArgs)
+      }
     },
 
     curryIt : function(fn) {
+      function applyArguments(_fn, args) {
+      return _fn.apply(null, args);
+    }
 
+    function getArgumentAccumulator(accumulatedArguments, expectedArgumentsCount) {
+      return function (currentArgument) {
+        accumulatedArguments.push(currentArgument);
+
+        var allArgumentsProvided = accumulatedArguments.length === expectedArgumentsCount;
+
+        if (allArgumentsProvided) {
+          return applyArguments(fn, accumulatedArguments);
+        }
+
+        return getArgumentAccumulator(accumulatedArguments, expectedArgumentsCount);
+      };
+    }
+
+    return getArgumentAccumulator([], fn.length);
     }
   };
 });
