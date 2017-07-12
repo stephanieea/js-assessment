0dfe8b11224aa7272686b62545e903cdf4524e57
Stephanie Andrews <seandrews11@gmail.com>
2017-07-12 12:09:15 -0600

    Refactor objects

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,10 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-      this.name = obj.name
-      this.greeting = obj.greeting
-      this.fn = fn
-      return this.fn()
+      return fn.call(obj)
     },
 
     alterObjects : function(constructor, greeting) {
-      this.constructor = constructor
-      this.greeting = greeting
       constructor.prototype.greeting = greeting
     },
 
